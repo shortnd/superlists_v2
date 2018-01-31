2fcb358e8d4827d447afe04f84744cb5c0afaeff
shortnd <ococncol@gmail.com>
2018-01-31 11:53:26 -0500

    Front page HTML now generated from template

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,7 @@
 """Functional Test for Superlist App"""
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 
@@ -33,13 +35,14 @@ class NewVisitorTest(unittest.TestCase):
 
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
-        inputbox.send_keys(Keys.Enter)
+        inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
         table = self.BROWSER.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows)
+            any(row.text == '1: Buy peacock feathers' for row in rows),
+            'New to-do item did not appear in the table'
         )
 
         # There is still a textbox inviting her to add another item. She
