7bf22d9f5e21a59000e228b8978319097ce1e3fa
shortnd <ococncol@gmail.com>
2018-02-07 12:38:56 -0500

    some test

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -2,7 +2,11 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
+from django.test import unittest
 import time
+
+MAX_WAIT = 10
 
 
 class NewVisitorTest(LiveServerTestCase):
@@ -12,12 +16,20 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.BROWSER.quit()
 
-    def check_for_row_in_list_table(self, row_text):
-        table = self.BROWSER.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.BROWSER.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as error:
+                if time.time() - start_time > MAX_WAIT:
+                    raise error
+                time.sleep(0.5)
 
-    def test_can_start_a_new_list_and_retrieve_it(self):
+    def test_can_start_a_list_for_one_user(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
         self.BROWSER.get(self.live_server_url)
@@ -41,8 +53,7 @@ class NewVisitorTest(LiveServerTestCase):
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
         # There is still a textbox inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very
@@ -50,21 +61,24 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox = self.BROWSER.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 
         # The page updates again, and now shows both items on her list
-        self.check_for_row_in_list_table('1: Buy peacock feathers')
-        self.check_for_row_in_list_table(
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+        self.wait_for_row_in_list_table(
             '2: Use peacock feathers to make a fly')
 
-        # Edith wonders wheter the site will remember her list. Then she sees
-        # that the site has generated a unique url for her -- there is some
-        # explanatory text to that effect.
-        self.fail('Finish the test!')
-
-        # She visits that URL - her to-do list is still there.
-
         # Satisfied, she goes back to sleep
+
+        def test_multiple_users_can_start_lists_at_different_urls(self):
+            # Edith starts a new to-do lists
+            self.BROWSER.get(self.live_server_url)
+            inputbox = self.BROWSER.find_element_by_id('id_new_item')
+            inputbox.send_keys('Buy peacock feathers')
+            inputbox.send_keys(Keys.ENTER)
+            self.wait_for_row_in_list_table('1: Buy peacock feathers')
+
+            # She notices that her list has a unique URL
+            edith_list_url = self.BROWSER.current_url
 
 
 if __name__ == '__main__':
